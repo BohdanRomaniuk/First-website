8d1f8337f93c0212a929e90677660f849bf4723f
BohdanRomaniuk <bohdan2307@gmail.com>
2017-10-01 22:38:53 +0300

    Add login system for internet shop

--- 2) Internet Shop/nginx/html/javascripts/app.js
+++ 2) Internet Shop/nginx/html/javascripts/app.js
@@ -13,28 +13,58 @@ angular.module('asusShop', [])
     console.log('Error: ' + error);
   });
   
+  // Delete a tovar
+  $scope.deleteTovar = (tovarID) => {
+	var sure = confirm("Ви впевнені що хочете видалити цей товар?");
+	if(sure==true)
+	{
+	  $http.delete('/server/index/' + tovarID)
+		.success((data) => {
+		$scope.allTovars = data;
+		console.log(data);
+      })
+      .error((data) => {
+		console.log('Error: ' + data);
+      });
+	}
+  };
+  
   // Create a new user
   $scope.createUser = () => {
     $http.post('/server/register', $scope.formData)
     .success((data) => {
       $scope.formData = {};
       $scope.allTovars = data;
+	  document.getElementById('status').innerHTML = "Ваш профіль успішно створено! </br> Увійдіть на сайт використовуючи свій логін та пароль";
+	  setTimeout(function() {$('#status').fadeOut('fast');}, 5000);
       console.log(data);
     })
     .error((error) => {
+	  document.getElementById('status').style.color = "red";
+	  document.getElementById('status').innerHTML = "Помилка під час реєстрації";
       console.log('Error: ' + error);
     });
   };
   
-  // Delete a tovar
-  $scope.deleteTovar = (tovarID) => {
-    $http.delete('/server/index/' + tovarID)
+  // Login user
+  $scope.loginUser = () => {
+    $http.post('/server/login', $scope.formData)
     .success((data) => {
-      $scope.allTovars = data;
+      $scope.formData = {};
+	  if(data==true)//Logined succesfully
+	  {
+		document.getElementById('status').innerHTML = "Ви успішно увійшли! </br> Вас буде переадресовано на головну сторінку";
+	    setTimeout(function() { window.location.replace("/index.html"); }, 5000);
+	  }
+	  else
+	  {
+	    document.getElementById('status').style.color = "red";
+		document.getElementById('status').innerHTML = "Такий користувач не існує!</br>Можливо ви помилилися при вводі паролю чи імені користувача";
+	  }
       console.log(data);
     })
-    .error((data) => {
-      console.log('Error: ' + data);
+    .error((error) => {
+      console.log('Error: ' + error);
     });
   };
 });
--- 2) Internet Shop/nodejs-server/server/routes/index.js
+++ 2) Internet Shop/nodejs-server/server/routes/index.js
@@ -35,7 +35,7 @@ router.delete('/server/index/:tovar_id', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     client.query('DELETE FROM tovars WHERE tovar_id=($1)', [id]);
-    var query = client.query('SELECT * FROM items ORDER BY id ASC');
+    var query = client.query('SELECT * FROM tovars ORDER BY tovar_id ASC');
     query.on('row', (row) => {
       results.push(row);
     });
@@ -61,7 +61,6 @@ router.post('/server/register', (req, res, next) => {
     query.on('row', (row) => {
       results.push(row);
     });
-    // After all data is returned, close connection and return results
     query.on('end', () => {
       done();
       return res.json(results);
@@ -69,86 +68,23 @@ router.post('/server/register', (req, res, next) => {
   });
 });
 
-router.post('/server/index', (req, res, next) => {
-  const results = [];
-  // Grab data from http request
-  const data = {text: req.body.text, complete: false};
-  // Get a Postgres client from the connection pool
+//USER LOGIN FORM
+router.post('/server/login', (req, res, next) => {
+  var isLoggined = false;
+  const data = {username: req.body.username, userpassword: req.body.userpassword};
   pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
     if(err) {
       done();
       console.log(err);
       return res.status(500).json({success: false, data: err});
     }
-    // SQL Query > Insert Data
-    client.query('INSERT INTO items(text, complete) values($1, $2)',
-    [data.text, data.complete]);
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM items ORDER BY id ASC');
-    // Stream results back one row at a time
+    const query = client.query('SELECT * FROM users WHERE username=($1) AND userpassword=($2) LIMIT 1', [data.username, data.userpassword]);
     query.on('row', (row) => {
-      results.push(row);
+      isLoggined = true;
     });
-    // After all data is returned, close connection and return results
     query.on('end', () => {
       done();
-      return res.json(results);
-    });
-  });
-});
-
-router.put('/server/index/:todo_id', (req, res, next) => {
-  const results = [];
-  // Grab data from the URL parameters
-  const id = req.params.todo_id;
-  // Grab data from http request
-  const data = {text: req.body.text, complete: req.body.complete};
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Update Data
-    client.query('UPDATE items SET text=($1), complete=($2) WHERE id=($3)',
-    [data.text, data.complete, id]);
-    // SQL Query > Select Data
-    const query = client.query("SELECT * FROM items ORDER BY id ASC");
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', function() {
-      done();
-      return res.json(results);
-    });
-  });
-});
-
-router.get('/server/users', (req, res, next) => {
-	const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM tovars ORDER BY tovar_id DESC;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
+      return res.json(isLoggined);
     });
   });
 });
